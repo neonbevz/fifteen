c2257a979889af2f24ca74aaeb1c885ee0871efa
Adriyan Bevz <neonbevz@gmail.com>
2019-08-17 16:02:40 +0300

    Add cell animations

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ let FieldHeight = 4;
 // Variables
 
 
+let Direction = Object.freeze({"none": 1, "right": 2, "up": 3, "left": 4, "down": 5});
 let GameAnimationState = Object.freeze({"idle": 1, "animating": 2});
 
 let CurrentGameAnimationState = GameAnimationState.idle;
@@ -26,7 +27,9 @@ let Field = [];
 
 let CellTemplate = (cellId, cellValue) => `
     <button id="${cellId}" class="game-cell" onclick="OnCellClick(this)">
-        ${cellValue}
+        <div class="game-cell__animated-content">
+            ${cellValue}
+        </div>
     </button>
 `;
 
@@ -124,7 +127,7 @@ function GenerateField() {
 
             // Create HTML element
 
-            let cellHtml = value === 0 ? EmptyCellTemplate("cell-" + id) : CellTemplate("cell-" + id, value);
+            let cellHtml = value === 0 ? EmptyCellTemplate("cell-" + value) : CellTemplate("cell-" + value, value);
             let cellElement = HtmlToElement(cellHtml);
             FieldElement.appendChild(cellElement);
 
@@ -163,7 +166,7 @@ function RenderField() {
  * @return {boolean}
  */
 function OnCellClick(cell) {
-    let cellValue = parseInt(cell.innerHTML);
+    let cellValue = parseInt(cell.firstElementChild.innerHTML);
     if (cellValue !== 0 && CurrentGameAnimationState === GameAnimationState.idle) {
         let cellPositionX, cellPositionY;
         for (let y = 0; y < FieldHeight; y++) {
@@ -175,12 +178,12 @@ function OnCellClick(cell) {
                 }
             }
         }
-        let emptyNeighbor = FindEmptyNeighbor(cellPositionX, cellPositionY);
+        let emptyNeighborDirection = FindEmptyNeighbor(cellPositionX, cellPositionY);
 
-        if (emptyNeighbor.length > 0) {
-            SwapCells([cellPositionX, cellPositionY], emptyNeighbor);
-        } else {
+        if (emptyNeighborDirection === Direction.none) {
             return false;
+        } else {
+            SwapCells([cellPositionX, cellPositionY], emptyNeighborDirection);
         }
         return true;
     } else {
@@ -189,22 +192,48 @@ function OnCellClick(cell) {
 }
 
 
+/**
+ * @return {number}
+ */
 function FindEmptyNeighbor(cellPositionX, cellPositionY) {
+    // console.log(cellPositionX, cellPositionY);
     if (cellPositionY > 0 && Field[cellPositionY - 1][cellPositionX].value === 0) {
-        return [cellPositionX, cellPositionY - 1];
+        // return [cellPositionX, cellPositionY - 1];
+        return Direction.up;
     } else if (cellPositionY < FieldHeight - 1 && Field[cellPositionY + 1][cellPositionX].value === 0) {
-        return [cellPositionX, cellPositionY + 1];
+        // return [cellPositionX, cellPositionY + 1];
+        return Direction.down;
     } else if (cellPositionX > 0 && Field[cellPositionY][cellPositionX - 1].value === 0) {
-        return [cellPositionX - 1, cellPositionY];
+        // return [cellPositionX - 1, cellPositionY];
+        return Direction.left;
     } else if (cellPositionX < FieldWidth - 1 && Field[cellPositionY][cellPositionX + 1].value === 0) {
-        return [cellPositionX + 1, cellPositionY];
+        // return [cellPositionX + 1, cellPositionY];
+        return Direction.right;
     } else {
-        return [];
+        // return [];
+        return Direction.none;
     }
 }
 
 
-function SwapCells(cellPosition, emptyCellPosition) {
+function SwapCells(cellPosition, emptyCellDirection) {
+    let emptyCellPosition;
+
+    switch (emptyCellDirection) {
+        case Direction.right:
+            emptyCellPosition = [cellPosition[0] + 1, cellPosition[1]];
+            break;
+        case Direction.up:
+            emptyCellPosition = [cellPosition[0], cellPosition[1] - 1];
+            break;
+        case Direction.left:
+            emptyCellPosition = [cellPosition[0] - 1, cellPosition[1]];
+            break;
+        case Direction.down:
+            emptyCellPosition = [cellPosition[0], cellPosition[1] + 1];
+            break;
+    }
+
     let cell = Field[cellPosition[1]][cellPosition[0]];
     let emptyCell = Field[emptyCellPosition[1]][emptyCellPosition[0]];
 
@@ -214,6 +243,30 @@ function SwapCells(cellPosition, emptyCellPosition) {
     emptyCell.element.parentNode.insertBefore(cell.element, emptyCell.element);
     temp.parentNode.insertBefore(emptyCell.element, temp);
     temp.parentNode.removeChild(temp);
+
+    let cellChild = cell.element.firstElementChild;
+    switch (emptyCellDirection) {
+        case Direction.right:
+            cellChild.style.transform = 'translateX(-100%)';
+            cellChild.offsetHeight; // Trick the engine into activating the transition
+            cellChild.style.transform = 'translateX(0)';
+            break;
+        case Direction.up:
+            cellChild.style.transform = 'translateY(100%)';
+            cellChild.offsetHeight; // Trick the engine into activating the transition
+            cellChild.style.transform = 'translateY(0)';
+            break;
+        case Direction.left:
+            cellChild.style.transform = 'translateX(100%)';
+            cellChild.offsetHeight; // Trick the engine into activating the transition
+            cellChild.style.transform = 'translateX(0)';
+            break;
+        case Direction.down:
+            cellChild.style.transform = 'translateY(-100%)';
+            cellChild.offsetHeight; // Trick the engine into activating the transition
+            cellChild.style.transform = 'translateY(0)';
+            break;
+    }
 
     Field[cellPosition[1]][cellPosition[0]] = emptyCell;
     Field[emptyCellPosition[1]][emptyCellPosition[0]] = cell;
